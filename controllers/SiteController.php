4a7eb11650f1248a5c4487b84e1f0a4449eee219
vitaliy shapovalov <kupper133@gmail.com>
2020-03-04 10:51:06 +0300

    add new main.php, add controller index, edit text view index

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,8 +61,7 @@ class SiteController extends Controller
      */
    public function actionIndex()
     {
-//        return $this->render('index');
-       return 'VideoSchool';
+        return $this->render('index');
     }
 
     /**
