353b5ef0b793733cfb108bfd7c6742b87d4d019f
vitaliy shapovalov <kupper133@gmail.com>
2020-03-07 00:47:30 +0300

    add boostrap and menu bar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,8 +1,42 @@
-<html>
-    <head>
-        <title>VideoSchool</title>
-    </head>
-    <body>
-        <?=$content?>
-    </body>
-</html>
+<?php
+
+    use yii\bootstrap\NavBar;
+
+?>
+<?php
+    $this->beginPage();
+?>
+
+    <html>
+        <head>
+            <title>VideoSchool</title>
+            <?php
+                $this->head();
+            ?>
+        </head>
+        <body>
+            <?php
+                $this->beginBody();
+            ?>
+            <?php
+                NavBar::begin([
+                    'brandLabel' => 'VideoSchool',
+                    'brandUrl' => Yii::$app->homeUrl,
+                    'options' => [
+                        'class' => 'navbar-default navbar-fixed-top'
+                    ]
+                ]);
+                NavBar::end();
+            ?>
+            <div class="container" style="margin-top:60px">
+                <?= $content ?>
+            </div>
+            <?php
+                $this->endBody();
+            ?>
+        </body>
+    </html>
+
+<?php
+    $this->endPage();
+?>
