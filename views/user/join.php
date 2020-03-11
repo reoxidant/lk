6cb9f5bec5289ce0aad05d0274742bd317ce3689
Soulmomental <kupper133@gmail.com>
2020-03-12 00:04:42 +0300

    style join and login pages

--- views/user/join.php
+++ views/user/join.php
@@ -1,3 +1,13 @@
 <?php ?>
-
-<h1>Join us</h1>
+<div class="panel panel-danger">
+    <div class="panel-heading">
+        <h1>Join us</h1>
+    </div>
+    <div class="panel-body">
+        name<br>
+        email<br>
+        password<br>
+        password2<br>
+        [Create]
+    </div>
+</div>
--- views/user/login.php
+++ views/user/login.php
@@ -1,3 +1,11 @@
 <?php ?>
-
-<h1>Log in</h1>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h1>Log in</h1>
+    </div>
+    <div class="panel-body">
+        email<br>
+        password<br>
+        [Create]
+    </div>
+</div>
