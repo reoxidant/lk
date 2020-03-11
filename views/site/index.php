e5a8ef436f70ba2f49b9c13797b964c5a4ffa8d6
Vitalii Voitnikov <kupper133@gmail.com>
2020-03-12 00:40:32 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Добро пожаловать, гость!</h1>
         <p class="lead">You have successfully created your Yii-powered application.</p>
-
+        <p><a class="btn btn-lg btn-success" href="https://github.com/reoxidant/lk/">GitHub Project</a></p>
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
