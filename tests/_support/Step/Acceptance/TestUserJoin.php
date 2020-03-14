09e1f56ff462c5eb8840c17ca12025376d7eca13
Soulmomental <kupper133@gmail.com>
2020-03-14 23:06:07 +0300

    fill actions test user join and login

--- tests/_support/Step/Acceptance/TestUserJoin.php
+++ tests/_support/Step/Acceptance/TestUserJoin.php
@@ -7,33 +7,55 @@ class TestUserJoin extends \AcceptanceTester
     public function imagineUser()
     {
         $I = $this;
-        $user = [];
+        $user = [
+            "name" => 'John',
+            "email" => 'vev@pub.osf.lt',
+            "password" => 'qwas'
+        ];
+        print_r($user);
         return $user;
     }
 
     public function joinUser($user)
     {
         $I = $this;
+        $I->amOnPage('/user/join');
+        $I->see('Join us');
+        $I->fillField('UserJoinForm[name]', $user['name']);
+        $I->fillField('UserJoinForm[email]', $user['email']);
+        $I->fillField('UserJoinForm[password]', $user['password']);
+        $I->fillField('UserJoinForm[password2]', $user['password']);
+        $I->click("Create");
     }
 
     public function loginUser($user)
     {
         $I = $this;
+        $I->amOnPage('/user/login');
+        $I->see('Log in');
+        $I->fillField('UserLoginForm[name]', $user['name']);
+        $I->fillField('UserLoginForm[email]', $user['email']);
+        $I->fillField('UserLoginForm[password]', $user['password']);
+        $I->fillField('UserLoginForm[password2]', $user['password']);
+        $I->click("Enter");
     }
 
     public function logoutUser()
     {
         $I = $this;
+        $I->click('Logout');
     }
 
     public function isUserLogged($user)
     {
         $I = $this;
+        $I->see($user['name']);
     }
 
     public function noUserLogged($user)
     {
         $I = $this;
+        $I->dontSee($user['name']);
     }
 
 }
