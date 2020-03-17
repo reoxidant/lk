dcce7591a332bd107b70c438f8dca6d25d6d5af3
Soulmomental <kupper133@gmail.com>
2020-03-17 23:33:01 +0300

    add migration db

--- migrations/m200316_185952_create_user_table.php
+++ migrations/m200316_185952_create_user_table.php
@@ -14,6 +14,10 @@ class m200316_185952_create_user_table extends Migration
     {
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'email' => $this->string()->notNull()->unique(),
+            'passhash' => $this->string()->notNull(),
+            'status' => $this->integer()->defaultValue(1)
         ]);
     }
 
