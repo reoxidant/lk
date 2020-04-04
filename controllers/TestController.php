38b5afeb1a553025dcf6f7ea57bc610c0fc90982
Soulmomental <kupper133@gmail.com>
2020-04-04 21:11:32 +0300

    comment old code

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -8,16 +8,17 @@ use yii\web\Controller;
 
 class TestController extends Controller
 {
-    public $defaultAction = 'my-test';
-
+//    public $defaultAction = 'my-test';
+/*
     public function actions(){
         return [
             'test' => 'app\components\HelloAction'
         ];
     }
+*/
 
-    public function actionIndex($name, $age = null){
-        var_dump($name, $age);
+    public function actionIndex($name = 'guest', $age = null){
+//        var_dump($name, $age);
         return '<h1>Hello World!</h1>';
     }
 
