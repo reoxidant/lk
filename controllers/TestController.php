a542c10ea0b5690b02080c27c6827f4be528f441
Soulmomental <kupper133@gmail.com>
2020-04-06 18:08:49 +0300

    add test controller new view - test

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -8,18 +8,29 @@ use yii\web\Controller;
 
 class TestController extends Controller
 {
-//    public $defaultAction = 'my-test';
-/*
-    public function actions(){
-        return [
-            'test' => 'app\components\HelloAction'
-        ];
-    }
-*/
+    //    public $defaultAction = 'my-test';
+    /*
+        public function actions(){
+            return [
+                'test' => 'app\components\HelloAction'
+            ];
+        }
+    */
 
-    public function actionIndex($name = 'guest', $age = null){
-//        var_dump($name, $age);
-        return '<h1>Hello World!</h1>';
+    public function actionIndex($name = 'guest', $age = 25)
+    {
+//        return $this->renderFile('@app/views/test/index.php');
+//        return $this->renderAjax('index');
+//        return $this->renderPartial('index');
+/*        return $this->render('index', [
+            'name' => $name,
+            'age' => $age
+        ]);*/
+
+        return $this->render(
+            'index',
+            compact('name', 'age')
+        );
     }
 
     public function actionMyTest(){
---
+++ views/test/index.php
@@ -0,0 +1,5 @@
+<h1>Hello World!</h1>
+<?= $this->render('inc')?>
+<?= $this->render('//inc/test.html')?>
+<p><?= $name ?></p>
+<p><?= $age ?></p>
