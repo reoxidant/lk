3ff7a0b04922ec3e4dba701e44e17fe34c73c083
Soulmomental <kupper133@gmail.com>
2020-04-09 21:46:00 +0300

    update input fields form and validation fields

--- models/EntryForm.php
+++ models/EntryForm.php
@@ -10,4 +10,21 @@ class EntryForm extends Model
     public $name;
     public $email;
     public $text;
+
+    public function rules()
+    {
+        return [
+          [['name', 'email', 'text'], 'required'],
+          ['email', 'email']
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+          'name' => 'Имя',
+          'email' => 'E-mail',
+          'text' => 'Текст:'
+        ];
+    }
 }
--- views/test/index.php
+++ views/test/index.php
@@ -1,19 +1,45 @@
 <?php
 
-use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 
 ?>
 
 <div class="col-md-12">
     <h2>Страница с формой!</h2>
-    <?php $form = \yii\bootstrap\ActiveForm::begin()?>
-        <?= $form->field($model, 'name');?>
-        <?= $form->field($model, 'email');?>
-        <?= $form->field($model, 'text')->textarea(['rows' => 3])?>
+    <?php $form = \yii\bootstrap\ActiveForm::begin([
+        'id' => 'my-form',
+        'enableClientValidation' => true, //false чтобы отключить валидацию
+        'options' => [
+            'class' => 'form-horizontal',
+        ],
+        'fieldConfig' => [
+          'template' => "
+                {label}\n
+                <div class='col-md-5'>{input}</div>\n
+                <div>{hint}</div>\n
+                <div class='col-md-5'>{error}</div>",
+            'labelOptions' => ['class' => 'col-md-2 control-label']
+        ]
+    ]) ?>
+    <?= $form->field($model, 'name')->hint('Заполните поле!')->textInput(['placeholder' => 'Введите имя'])->label('Имя:'); ?>
 
-        <div class="form-group">
-            <?= Html::submitButton('Отправить', ['class' => 'btn btn-default'])?>
+    <?= $form->field($model, 'email')->input('email', ['placeholder' => 'Введите email']); ?>
+
+    <?= $form->field($model, 'text', [
+        'template' => "
+                {label}\n
+                
+                <div class='col-md-5'>{input}</div>\n
+                <div>{hint}</div>\n
+                <div class='col-md-2'>Пример конфигурации шаблона поле для формы</div>
+                <div class='col-md-3'>{error}</div>",
+        'labelOptions' => ['class' => 'col-md-2 control-label']
+    ])->textarea(['rows' => 7, 'placeholder' => 'Введите текст'])?>
+
+    <div class="form-group">
+        <div class="col-md-5 col-md-offset-2">
+            <?= Html::submitButton('Отправить', ['class' => 'btn btn-default btn-block']) ?>
         </div>
-    <?php \yii\bootstrap\ActiveForm::end()?>
+    </div>
+    <?php \yii\bootstrap\ActiveForm::end() ?>
 </div>
