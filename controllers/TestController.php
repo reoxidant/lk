13165dbd99a6de103cd19e38d4e95ea7dc31e2bb
Soulmomental <kupper133@gmail.com>
2020-04-10 16:18:08 +0300

    add prefix to db + add new view country + add new model + add new layout + get parameters from controller

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\Country;
 use app\models\EntryForm;
 use Yii;
 use yii\web\Response;
@@ -30,6 +31,59 @@ class TestController extends AppController
         }
 
         return $this->render('index', compact('model'));
+    }
+
+    public function actionCountry()
+    {
+        $this->layout = 'country';
+        $this->view->title = "Тестовая модель бд Country";
+
+        //Получение данных, строковый формат
+//        $countries = Country::find()->all();
+//        $countries = Country::find()->where("population < 100000000 AND code <> 'DE'")->all();
+
+        //Защита от SQL инъекции, привязка параметров
+        /*$countries = Country::find()->where(
+
+            "population < :population AND code <> :code",
+            [
+                ':population' => 100000000,
+                ':code' => 'DE'
+            ]
+        )->all();*/
+
+        //Получение данных, формат массива
+        /*$countries = Country::find()->where(
+            [
+                'code' => ['DE', 'FR', 'AU'],
+                'status' => 1
+            ]
+        )->all();*/
+
+        //Получение данных, Формат операторов
+        /*$countries = Country::find()->where(
+            ['like', 'name', 'ni']
+        )->all();*/
+
+//        $countries = Country::find()->orderBy('population DESC')->all();
+
+        //Получаем число запись
+//        $countries = Country::find()->count();
+//        debug($countries, 1);
+
+        //Получаем данные одной запись
+//        $countries = Country::find()->limit(1)->where(['code' => 'RU'])->one();
+
+        //Аналог функции Country::find()->all();
+//        $countries = Country::findAll(['DE', 'RU', "CN"]);
+
+        //Аналог функции Country::find()->one();
+//        $countries = Country::findOne(['code' => 'DE', 'status' => 1]);
+
+        //Экононмим память, выводя не обьет, а массив
+        $countries = Country::find()->asArray()->all();
+
+        return $this->render('country', compact('countries'));
     }
 
     public function actionMyTest()
---
+++ views/test/country.php
@@ -0,0 +1,34 @@
+<div class="col-md-12">
+    <h1>Тестовая модель бд Country</h1>
+    <?php //debug($countries, 1)?>
+
+    <? if (is_object($countries)): ?>
+        <table class="table">
+            <?php foreach ($countries as $country): ?>
+                <tr>
+                    <td><?= $country->code ?></td>
+                    <td><?= $country->name ?></td>
+                    <td><?= $country->population ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <? elseif (is_array($countries)): ?>
+        <table class="table">
+            <?php foreach ($countries as $country): ?>
+                <tr>
+                    <td><?= $country['code'] ?></td>
+                    <td><?= $country['name'] ?></td>
+                    <td><?= $country['population'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <? else: ?>
+        <table class="table">
+            <tr>
+                <td><?= $countries->code ?></td>
+                <td><?= $countries->name ?></td>
+                <td><?= $countries->population ?></td>
+            </tr>
+        </table>
+    <? endif; ?>
+</div>
